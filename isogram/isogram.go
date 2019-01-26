8d847a7a23b2bd5978dcd27c886f62052a6441c7
Matthew Harrilal <matthewharrilal@gmail.com>
2019-01-26 08:21:32 -0800

    Finished isogram exercise and ran against tests

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,19 +1,25 @@
 package isogram
 
-func DetermineIsogram(word string) bool {
-	frequencyCount := make(map[string]int) // Containing the frequency count of the letters in the world
+import (
+	"fmt"
+	"strings"
+)
 
+func IsIsogram(word string) bool {
+	frequencyCount := make(map[string]int) // Containing the frequency count of the letters in the world
+	word = strings.ToLower(word)
 	for _, letter := range word {
 		// Ignore spaces and hyphens
-		if letter != ' ' && letter == '-' { // Using single quotes to denote character as opposed to string
+		if letter != ' ' && letter != '-' { // Using single quotes to denote character as opposed to string
 			_, ok := frequencyCount[string(letter)]
 
 			if ok { // Meaning callback of operation returned true
 				return false
 			} else {
-				frequencyCount[string(letter)] ++ 
+				frequencyCount[string(letter)]++
 			}
 		}
 	}
+	fmt.Println(frequencyCount)
 	return true
 }
