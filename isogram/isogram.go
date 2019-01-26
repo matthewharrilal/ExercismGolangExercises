6e7decaa9af80aa6db91073d9bf46e3c4b1021da
Matthew Harrilal <matthewharrilal@gmail.com>
2019-01-26 08:14:20 -0800

    Completed isogram exercise have to run against tests

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -6,8 +6,14 @@ func DetermineIsogram(word string) bool {
 	for _, letter := range word {
 		// Ignore spaces and hyphens
 		if letter != ' ' && letter == '-' { // Using single quotes to denote character as opposed to string
-			
+			_, ok := frequencyCount[string(letter)]
+
+			if ok { // Meaning callback of operation returned true
+				return false
+			} else {
+				frequencyCount[string(letter)] ++ 
+			}
 		}
 	}
-
+	return true
 }
