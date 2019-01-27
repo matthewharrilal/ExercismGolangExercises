490f867b5bb8145798b1cf7120c7919642739f03
Matthew Harrilal <matthewharrilal@gmail.com>
2019-01-27 11:10:42 -0800

    Didnt finish the luhn algo and switched to independent mode

---
+++ luhn/luhn.go
@@ -0,0 +1,10 @@
+package luhn
+
+func Luhn(creditString string) bool { // Return boolean value depending if the credit string is valid
+
+	for index, val := range creditString {
+		if index % 2 == 0 {
+			
+		}
+	}
+}
