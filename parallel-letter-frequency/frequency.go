c1880dac9385984379c5a40cddaa2631492af4b6
Matthew Harrilal <matthewharrilal@gmail.com>
2019-01-31 09:12:20 -0800

    Most recent push of finding histogram in parallel

--- parallel-letter-frequency/frequency.go
+++ parallel-letter-frequency/frequency.go
@@ -1,14 +1,43 @@
 package letter
 
+import "fmt"
+
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
 // Frequency counts the frequency of each rune in a given text and returns this
 // data as a FreqMap.
 func Frequency(s string) FreqMap {
-	m := FreqMap{}
+	m := FreqMap{} // Instantiating the map to contain the frequency
 	for _, r := range s {
-		m[r]++
+		m[r]++ // Increment to the value of occurences
 	}
 	return m
+}
+
+func ConcurrentFrequency(texts []string) FreqMap {
+	// Concurrently calculates the frequency of occurences of letters in a string
+	channel := make(chan FreqMap) // Going to be writing the frequency of occurences of the letters through the channels
+	result := FreqMap{}
+	for _, word := range texts {
+		// Once we are able to calculate the value of the letter we are currently on we want to be
+
+		go func(word string) {
+			//fmt.Println(Frequency(word))
+			channel <- Frequency(word) // Write histogram to channel for each word
+		}(word) // Execute anonymous func
+	}
+
+	// SO what do we know, that texts is an array of words
+
+	for range texts { // Iterate over the words in the texts
+		//fmt.Println(texts)
+		for val := range <-channel {
+			fmt.Println(val)
+			//result[letter] ++
+		}
+	}
+
+	fmt.Println(result)
+	return result
 }
