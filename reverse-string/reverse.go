89e13a87f041c6577a5f6dd7a65d855848177ba7
Matthew Harrilal <matthewharrilal@gmail.com>
2019-01-31 10:16:09 -0800

    Finished the reversing string function

---
+++ reverse-string/reverse.go
@@ -0,0 +1,28 @@
+package reverse
+
+import (
+	"fmt"
+	"strings"
+)
+
+func reverse(word string) string {
+	// Firt convert the word to an array slice
+	arrWord := strings.Split(word, "")
+
+	if word == "" {
+		return ""
+	}
+
+	left := 0
+	right := len(word) - 1
+
+	fmt.Println(arrWord[left], arrWord[right])
+
+	for left < right {
+		arrWord[left], arrWord[right] = arrWord[right], arrWord[left]
+
+		left++
+		right--
+	}
+	return strings.Join(arrWord, "")
+}
